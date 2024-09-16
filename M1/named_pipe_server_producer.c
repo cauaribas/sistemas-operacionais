92a9f62a197eaa6846fb789b6c849ced8339a493
Cauã Ribas <100866872+RibasOne@users.noreply.github.com>
2024-09-16 20:45:57 +0000

    Sync: Sync Files

--- M1/named_pipe_server_producer.c
+++ M1/named_pipe_server_producer.c
@@ -10,25 +10,25 @@
 #include <sys/un.h>
 #include <sys/select.h>
 
-#define THREAD_POOL_SIZE 2
+// Constantes
+#define THREAD_POOL_SIZE 4
 #define STRING_SOCK_PATH "/tmp/string_pipeso"
 #define NUMBER_SOCK_PATH "/tmp/number_pipeso"
 
+// Struct para armazenar as tarefas a serem executadas por threads, contendo a função e o socket do cliente
 typedef struct Task {
     void (*taskFunction)(int);
     int client_socket;
 } Task;
 
+// Variáveis globais compartilhadas entre threads
 Task taskQueue[256];
 int taskCount = 0;
-
 pthread_mutex_t mutexQueue;
 pthread_cond_t condQueue;
 
-struct timespec startTime, endTime;
-
 void executeTask(Task* task){
-    task->taskFunction(task->client_socket);
+    task->taskFunction(task->client_socket); // Executa a função de tarefa associada
 }
 
 void submitTask(Task task){
@@ -44,19 +44,19 @@ void* startThread(void* args){
         Task task;
 
         pthread_mutex_lock(&mutexQueue);
-        while(taskCount == 0){
+        while(taskCount == 0){ // Se não há tarefas, espera até ser sinalizado
             pthread_cond_wait(&condQueue, &mutexQueue);
         }
 
+        // Retira a primeira tarefa da fila
         task = taskQueue[0];
         for(int i = 0; i < taskCount - 1; i++){
             taskQueue[i] = taskQueue[i + 1];
         }
         taskCount--;
-        // printf("Task removida. Tarefas restantes na fila: %d\n", taskCount);
         pthread_mutex_unlock(&mutexQueue);
         
-        executeTask(&task); 
+        executeTask(&task); // Executa a tarefa removida da fila
     }
 }
 
@@ -64,24 +64,28 @@ int startServer(const char* sockpath){
     int server_socket;
     struct sockaddr_un local;
 
+    // Criação do socket UNIX
     server_socket = socket(AF_UNIX, SOCK_STREAM, 0);
     if (server_socket < 0) {
         perror("Falha ao criar socket");
         return 1;
     }
 
+    // Bind socket no endereço local
     memset(&local, 0, sizeof(local));
     local.sun_family = AF_UNIX;
-    strncpy(local.sun_path, sockpath, sizeof(local.sun_path) - 1);
-    unlink(local.sun_path);
+    strncpy(local.sun_path, sockpath, sizeof(local.sun_path) - 1); // Define o caminho do socket
+    unlink(local.sun_path); // Remove qualquer socket anterior com o mesmo caminho
     int len = strlen(local.sun_path) + sizeof(local.sun_family);
     
+    // Vincula o socket ao caminho especificado
     if (bind(server_socket, (struct sockaddr*)&local, len) < 0) {
         perror("Falha ao fazer bind no socket");
         close(server_socket);
         return 1;
     }
 
+    // Coloca o socket em modo de escuta por conexões
     if (listen(server_socket, 5) < 0) {
         perror("Erro ao escutar socket");
         close(server_socket);
@@ -93,7 +97,6 @@ int startServer(const char* sockpath){
 
 void handleStringConnection(int client_socket){    
     char buffer[1024];
-    // sleep(2);
 
     // Ler dados do cliente
     if (read(client_socket, buffer, sizeof(buffer)) < 0) {
@@ -114,7 +117,6 @@ void handleStringConnection(int client_socket){
 
 void handleNumberConnection(int client_socket){    
     char buffer[1024];
-    // sleep(2);
 
     // Ler dados do cliente
     if (read(client_socket, buffer, sizeof(buffer)) < 0) {
@@ -141,6 +143,7 @@ void acceptConnection(int socket, void (*handler)(int)){
     if (client_socket >= 0) {
         printf("Nova conexão recebida\n");
         
+        // Cria uma nova tarefa e a submete para o pool de threads
         Task t = {
             .taskFunction = handler, 
             .client_socket = client_socket
@@ -151,25 +154,26 @@ void acceptConnection(int socket, void (*handler)(int)){
 }
 
 int main(int argc, char* argv[]){
-    pthread_t th_pool[THREAD_POOL_SIZE];
+    pthread_t th_pool[THREAD_POOL_SIZE]; // Array de threads
     int string_socket, number_socket;
     
     pthread_mutex_init(&mutexQueue, NULL);
     pthread_cond_init(&condQueue, NULL);
 
+    // Cria as threads do pool
     for(int i = 0; i < THREAD_POOL_SIZE; i++){
         if(pthread_create(&th_pool[i], NULL, &startThread, NULL)){
             perror("Failed to create thread");
         }
     }
     
+    // Inicia os servidores para strings e números
     string_socket = startServer(STRING_SOCK_PATH);
     number_socket = startServer(NUMBER_SOCK_PATH);
 
     printf("Servidor escutando em %s e %s...\n", STRING_SOCK_PATH, NUMBER_SOCK_PATH);
 
-    clock_gettime(CLOCK_MONOTONIC, &startTime);
-
+    // Define variáveis para o select e determina o maior descritor de arquivo
     fd_set read_fds;
     int max_fd = (string_socket > number_socket) ? string_socket : number_socket;
 
@@ -178,6 +182,7 @@ int main(int argc, char* argv[]){
         FD_SET(string_socket, &read_fds); // Adiciona string_socket ao conjunto
         FD_SET(number_socket, &read_fds); // Adiciona number_socket ao conjunto
 
+        // Espera por atividade nos sockets com select
         int activity = select(max_fd + 1, &read_fds, NULL, NULL, NULL);
         if (activity < 0) {
             perror("Erro no select");
@@ -193,9 +198,9 @@ int main(int argc, char* argv[]){
         }
     }
     
+    // Limpando recursos (devido ao loop infinito, nunca chegará aqui)
     pthread_mutex_destroy(&mutexQueue);
     pthread_cond_destroy(&condQueue);
-
     close(string_socket);
     close(number_socket);
     return 0;
