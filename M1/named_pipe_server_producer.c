f262f4e28767ae3e4f5d342ba572c1e74343f684
Cauã Ribas <100866872+RibasOne@users.noreply.github.com>
2024-09-17 13:05:17 +0000

    Sync: Sync Files

--- M1/named_pipe_server_producer.c
+++ M1/named_pipe_server_producer.c
@@ -11,7 +11,7 @@
 #include <sys/select.h>
 
 // Constantes
-#define THREAD_POOL_SIZE 4
+#define THREAD_POOL_SIZE 16
 #define STRING_SOCK_PATH "/tmp/string_pipeso"
 #define NUMBER_SOCK_PATH "/tmp/number_pipeso"
 
@@ -141,8 +141,6 @@ void acceptConnection(int socket, void (*handler)(int)){
     socklen_t len = sizeof(remote);
     int client_socket = accept(socket, (struct sockaddr*)&remote, &len);
     if (client_socket >= 0) {
-        printf("Nova conexão recebida\n");
-        
         // Cria uma nova tarefa e a submete para o pool de threads
         Task t = {
             .taskFunction = handler, 
--- M1/test_clients.sh
+++ M1/test_clients.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-NUM_CLIENTS=500
+NUM_CLIENTS=1000
 WORD="palavra_teste"  # Defina a palavra que você quer enviar
+ITERATIONS=10
+TOTAL_TIME=0
 
-# Medir o tempo de execução das conexões
-time {
-  for ((i = 1; i <= NUM_CLIENTS; i++))
-  do
-    echo "$WORD" | ./string_consumer & 
-  done
-  wait  # Espera que todos os clientes finalizem
+for ((n = 1; n <= ITERATIONS; n++))
+do
+  # Executa o comando e captura o tempo real (real) de execução
+  echo "Execução $n"
+  EXEC_TIME=$( { time ( 
+    for ((i = 1; i <= NUM_CLIENTS; i++))
+    do
+      echo "$WORD" | ./string_consumer &
+      echo $i | ./number_consumer &
+    done
+    wait
+  ) 2>&1; } | grep real | awk '{print $2}' )
 
-  for ((i = 1; i <= NUM_CLIENTS; i++))
-  do
-    echo $i | ./number_consumer & 
-  done
-  wait  # Espera que todos os clientes finalizem
-}
+  # Converte o tempo de minutos:segundos.milisegundos para segundos
+  MIN=$(echo $EXEC_TIME | awk -F'm' '{print $1}')
+  SEC=$(echo $EXEC_TIME | awk -F'm' '{print $2}' | sed 's/s//')
+  EXEC_TIME_SEC=$(echo "$MIN * 60 + $SEC" | bc)
+  
+  # Soma ao tempo total
+  TOTAL_TIME=$(echo "$TOTAL_TIME + $EXEC_TIME_SEC" | bc)
+  echo "Tempo da execução $n: $EXEC_TIME_SEC segundos"
+done
+
+# Calcula a média
+AVERAGE_TIME=$(echo "$TOTAL_TIME / $ITERATIONS" | bc -l)
+echo "Média de tempo após $ITERATIONS execuções: $AVERAGE_TIME segundos"
